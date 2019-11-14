205606131d6fa97296bc138a17892031ee3adcaf
Cesar <cdgparedes112@hotmail.com>
2019-11-13 22:13:27 -0500

    deleting files hexagonal architeture

--- src/main/java/dominio/repositorio/PeliculaRepository.java
+++
@@ -1,9 +0,0 @@
-package dominio.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.com.ceiba.tiquetes.cesar.grajales.dominio.Pelicula;
-
-public interface PeliculaRepository  extends CrudRepository<Pelicula,Long>{
-	Pelicula findBynombrePelicula(String nombrePelicula);
-}
--- src/main/java/dominio/servicio/IPeliculaService.java
+++
@@ -1,9 +0,0 @@
-package dominio.servicio;
-
-import java.util.List;
-
-import dominio.entidad.Pelicula;
-
-public interface IPeliculaService {
-	public List<Pelicula> obtenerListaPeliculas();
-}
